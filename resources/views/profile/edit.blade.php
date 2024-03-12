523a5f035dc14d43191c0e99557cda0fe57fedd2
igusil <igusil@outlook.com>
2024-03-11 21:58:53 -0300

    profile edit

---
+++ resources/views/profile/edit.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Editando perfil</title>
+</head>
+<body>
+  
+  <center>
+    <h2>Editando seu perfil</h2>
+    <small>{{Auth::user()->name}}, Você está editando o seu perfil</small>
+  </center>
+
+</body>
+</html>
