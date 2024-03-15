4736e2f84d1424e73f975cb2381ac655e4f722e8
igusil <igusil@outlook.com>
2024-03-15 19:19:03 -0300

    csrf edit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
         return view('profile.edit');
     }
     
-    public function  update(Request $request) {
+    public function update(Request $request){
         
         $user = User::find($request->id);
         if ($user){
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -12,14 +12,14 @@
     <h2>Editando seu perfil</h2>
     <small>{{Auth::user()->name}}, Você está editando o seu perfil</small>
     
-    <form action=""/profile/update" method="post">
+    <form action="/profile/update" method="POST">
       @csrf
       <input type="hidden" name="id" value="{{Auth::user()->id}}">
+
       <input type="text" name="name" value="{{Auth::user()->name}}">
       <br/>
       <input type="email" name="email" value="{{Auth::user()->email}}">
       <br/>
-      <!--<input type="password" name="password" value="{{Auth::user->password}}">-->
       <input type="submit" value="Atualizar perfil">
     </form>
   </center>
