50fa2ed4c8395de96002b771b1db86102f2ef4d9
igusil <igusil@outlook.com>
2024-03-17 20:03:51 -0300

    app blade

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -12,15 +12,22 @@
     <h2>Editando seu perfil</h2>
     <small>{{Auth::user()->name}}, Você está editando o seu perfil</small>
     
-    <form action="/profile/update" method="POST">
+    <form action="/profile/update" method="POST" enctype="multipart/form-data">
       @csrf
       <input type="hidden" name="id" value="{{Auth::user()->id}}">
 
-      <input type="text" name="name" value="{{Auth::user()->name}}">
+      Name: <input type="text" name="name" value="{{Auth::user()->name}}">
       <br/>
-      <input type="email" name="email" value="{{Auth::user()->email}}">
+      <br/>
+      E-mail: <input type="email" name="email" value="{{Auth::user()->email}}">
+      <br/>
+      <br/>
+      Upload da foto: <input type="file" value="photo_path">
+      <br/>
       <br/>
       <input type="submit" value="Atualizar perfil">
+
+      
     </form>
   </center>
 
