e10da96cf73f0363d8c3c9a6c9963f69f54283dd
igusil <igusil@outlook.com>
2024-03-21 22:33:51 -0300

    Unrestricted file upload

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
 
     public function update(Request $request){
 
+        //verificar se a extemsão do arquivo é .png/.jpg/.jpeg
         
         $data = $request->all();
 
